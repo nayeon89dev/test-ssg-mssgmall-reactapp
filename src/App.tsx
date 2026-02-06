299eadab07cbcfa3ee18ead5696b52f367669941
원나연(193664) / 서비스개발담당 WEB개발팀 <won.nayeon@ssg.com>
2026-02-06 10:24:55 +0900

    useEffect 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -7,6 +7,9 @@ function App() {
 	const [count, setCount] = useState(0);
 	const [name, setName] = useState("");
 
+	useEffect(() => {
+		console.log("name", name);
+	}, [name]);
 	return (
 		<>
 			<div>
